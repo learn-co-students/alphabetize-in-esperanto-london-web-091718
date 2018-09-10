5362c05c2fd48c381620624972e7d1af8a69b094
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-10 20:44:23 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,24 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  esperanto_letter = {}
+  i = 1
+  ESPERANTO_ALPHABET.each_char do |letter| 
+    esperanto_letter[letter] = i
+    i += 1
+  end
+  
+  
+  
+  arr.sort_by do |element| 
+    key = []
+    element.each_char do |letter|
+      if esperanto_letter.key?(letter)
+        key << esperanto_letter[letter]
+      else
+        key << 0
+      end
+    end
+    key
+  end
 end
